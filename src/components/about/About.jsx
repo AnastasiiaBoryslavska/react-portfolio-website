7f4cf95e13f04b07e869b870cb7c776cc99af0b1
Anastasiia Boryslavska <anastasiiaforwork@gmail.com>
2022-06-15 22:54:29 -0400

    about component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,55 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {GiDiploma} from 'react-icons/gi'
+import {VscFolderLibrary} from 'react-icons/vsc'
+
 const About = () => {
   return (
-    <section id="about">About</section>
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className='container about__container'>
+        <div className='about__me'>
+          <div className='about__me-image'>
+            <img src={ME} alt="About Image"/>
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className='about__cards'>
+            <article className="about__card">
+               <FaAward className='about__icon'/>
+               <h5>Experience</h5>
+               <small>2+ Years working</small>
+            </article> 
+
+            <article className="about__card">
+               <GiDiploma className='about__icon'/>
+               <h5>Education</h5>
+               <small>Master's in Computer Science</small>
+            </article> 
+
+            <article className="about__card">
+               <VscFolderLibrary className='about__icon'/>
+               <h5>Projects</h5>
+               <small>10+ projects completed</small>
+            </article> 
+          </div>
+
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna
+          aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat.  
+          </p>
+          <a href="#contact" className="btn btn-primary">Let's talk</a>
+        </div>
+      </div>
+  
+    </section>
   )
 }
 
